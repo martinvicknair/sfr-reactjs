4fd0ed5a034e25fa097f1258c80e7af03e6b1012
Martin Vicknair <martinvicknair@gmail.com>
2020-02-22 07:40:05 -0700

    .env working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+require('dotenv').config();
+
+console.log(require('dotenv').config());
 
 const mapStyles = {
-  width: '100%',
-  height: '100%'
+  width: '80%',
+  height: '80%'
 };
 
 export class MapContainer extends Component {
@@ -11,11 +14,11 @@ export class MapContainer extends Component {
     return (
       <Map
         google={this.props.google}
-        zoom={14}
+        zoom={8}
         style={mapStyles}
         initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
+         lat: 39.742043,
+         lng: -104.991531
         }}
       />
     );
@@ -23,5 +26,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.MAP_API_KEY
+  apiKey: process.env.REACT_APP_MAP_API_KEY
 })(MapContainer);
